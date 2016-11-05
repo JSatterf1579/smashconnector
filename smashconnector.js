b556f047b9fff61559d6b0cbf3ff2ed1354ae1a8
drew-myers <drewmyers1620@gmail.com>
2016-11-05 11:29:22 -0400

    adding to github

---
+++ smashconnector.js
@@ -0,0 +1,195 @@
+var MongoClient = require('mongodb').MongoClient;
+var levenshtein = require('fast-levenshtein');
+
+//Mongo connection section
+
+const playerCollection = "players";
+const stageCollection = "stages";
+const gameTypeCollection = "gameTypes";
+const gameCollection = "games";
+
+function insertGames(url, games, callback) {
+	mongoInsert(url, gameCollection, games, callback);
+}
+
+function listGames(url, query, callback) {
+	mongolist(url, gameCollection, query, callback);
+}
+
+function insertStages(url, stages, callback) {
+	mongoInsert(url, stageCollection, stages, callback);
+}
+
+function listStages(url, query, callback) {
+	mongoList(url, stageCollection, query, callback);
+}
+
+function insertGameTypes(url, gameTypes, callback) {
+	mongoInsert(url, gameTypeCollection, gameTypes, callback);
+}
+
+function listGameTypes(url, query, callback) {
+	mongoList(url, gameTypeCollection, query, callback);
+}
+
+function insertPlayers(url, players, callback) {
+	mongoInsert(url, playerCollection, players, callback);
+}
+
+function listPlayers(url, query, callback) {
+	mongoList(url, playerCollection, query, callback);
+}
+
+function mongoList(url, collectionName, query, callback) {
+	MongoClient.connect(url, (err, db) => {
+		var collection = db.collection(collectionName);
+		collection.find(query).toArray((err, docs) => {
+			db.close();
+			callback(docs);
+		});
+	});
+}
+
+function mongoInsert(url, collectionName, docs, callback) {
+	MongoClient.connect(url, (err, db) => {
+		var collection = db.collection(collectionName);
+		collection.insert(docs, (err, result) => {
+			db.close();
+			callback(result);
+		});
+	});
+}
+
+//Data validation section
+
+function match(name, list) {
+	//Null check
+	if(!(name && list)) {
+		return {
+			"match": false,
+			"suggestions": []
+		};
+	}
+
+	var result = {};
+	if(isValidName(name, list)) { //Given name is an exact match
+		result.match = true,
+		result.suggestions = [];
+	} else { //Match for name not found, return suggestions
+		result.match = false;
+		result.suggestions = findSubstringNames(name, list);
+		result.suggestions = result.suggestions.concat(findSimilarNames(name, list));
+	}
+	return result;
+}
+
+function isValidName(name, list) {
+	//Null check
+	if(!(name && list)) { return false; }
+
+	for (var i in list) {
+  		if(name.toLowerCase() === list[i].name.toLowerCase()) {
+  			return true;
+  		}
+	}
+	return false;
+}
+
+function findSubstringNames(name, list) {
+	//Null check
+	if(!(name && list)) { return []; }
+
+	var result = [];
+	for (var i in list) {
+  		if (~list[i].name.toLowerCase().indexOf(name.toLowerCase())) {
+  			result.push(list[i].name);
+  		}
+	}
+	return result;
+}
+
+function findSimilarNames(name, list) {
+	//Null check
+	if(!(name && list)) { return []; }
+
+	var result = [];
+	for (var i in list) {
+  		if((levenshtein.get(name.toLowerCase(), list[i].name.toLowerCase())/name.length) < 0.5) {
+  			result.push(list[i].name);
+  		}
+	}
+	return result;
+}
+
+//Data formatting section
+
+function formatParticipantData(name, character, starting_stocks, remaining_stocks, win)
+{
+	if(isInputEmpty([name, character, starting_stocks, remaining_stocks, win])) {
+		return new Error("empty input provided");
+	}
+
+	return {
+		"name": name,
+		"character": character,
+		"starting_stocks": starting_stocks,
+		"remaining_stocks": remaining_stocks,
+		"win": win
+	}
+}
+
+function formatStageData(name, chooser)
+{
+	if(isInputEmpty([name, chooser])) {
+		return new Error("empty input provided");
+	}
+
+	return {
+		"name": name,
+		"chooser": chooser
+	}
+}
+
+function formatGameData(participantList, stage, gameType)
+{
+	if(isInputEmpty([participantList, stage, gameType])) {
+		return new Error("empty input provided");
+	}
+
+	if(participantList.constructor !== Array) {
+		return new Error("list of participants expected but no array provided")
+	}
+
+	return {
+		"participants": participantList,
+		"stage": stage,
+		"gameType": gameType
+	}
+}
+
+function isInputEmpty(inputList) {
+	for(var i in inputList) {
+		if(!(inputList[i] && inputList[i] !== "")) {
+			return true;
+		}
+	}
+	return false;
+}
+
+module.exports = {
+	insertGames: insertGames,
+	listGames: listGames,
+	insertStages: insertStages,
+	listStages: listStages,
+	insertGameTypes: insertGameTypes,
+	listGameTypes: listGameTypes,
+	listPlayers: listPlayers,
+	insertPlayers: insertPlayers,
+	match: match,
+	isValidName: isValidName,
+	findSubstringNames: findSubstringNames,
+	findSimilarNames: findSimilarNames,
+	formatParticipantData: formatParticipantData,
+	formatStageData: formatStageData,
+	formatGameData: formatGameData
+}
---
+++ spec.js
@@ -0,0 +1,300 @@
+var chai = require('chai');
+var smashconnector = require('./smashconnector');
+
+describe('data validation', () => {
+
+	describe('name validation', () => {
+		it('returns true if a name is found', () => {
+			var list = [
+				{ "name": "jim" },
+				{ "name": "bob" }
+			]
+
+			var isValid = smashconnector.isValidName("jim", list);
+			chai.expect(isValid).to.equal(true, 'method did not return true for name in list');
+		});
+
+		it('is not case sensitive', () => {
+			var list = [
+				{ "name": "jim" },
+				{ "name": "bob" }
+			]
+
+			var isValid = smashconnector.isValidName("BOB", list);
+			chai.expect(isValid).to.equal(true, 'method did not return true for ALL CAPS name in list');
+		});
+
+		it('returns false if a name is not found', () => {
+			var list = [
+				{ "name": "jim" },
+				{ "name": "bob" }
+			]
+
+			var isValid = smashconnector.isValidName("tim", list);
+			chai.expect(isValid).to.equal(false, 'method did not return false for name not in list');
+		});
+
+		it('is robust to null input', () => {
+			var isValid = smashconnector.isValidName("tim", null);
+			chai.expect(isValid).to.equal(false, 'method did not return false for null list');
+
+			var list = [
+				{ "name": "jim" },
+				{ "name": "bob" }
+			]
+			var isValid = smashconnector.isValidName(null, list);
+			chai.expect(isValid).to.equal(false, 'method did not return false for null name');
+		})
+	});
+
+	describe('name substring matching', () => {
+		it('returns superset name if one is found', () => {
+			var list = [
+				{ "name": "jim-bob" },
+				{ "name": "bob" }
+			]
+
+			var result = smashconnector.findSubstringNames("jim", list);
+			chai.expect(result).to.deep.equal(['jim-bob'], 'method did not return superset name for substring');
+		});
+
+		it('returns multiple superset names if multiple are found', () => {
+			var list = [
+				{ "name": "jim-bob" },
+				{ "name": "bob" },
+				{ "name": "jimmy" }
+			]
+
+			var result = smashconnector.findSubstringNames("jim", list);
+			chai.expect(result).to.deep.equal(['jim-bob', 'jimmy'], 'method did not return multiple superset names for multiple substrings');
+		});
+
+		it('is not case sensitive', () => {
+			var list = [
+				{ "name": "jim-bob" },
+				{ "name": "bob" }
+			]
+
+			var result = smashconnector.findSubstringNames("JIM", list);
+			chai.expect(result).to.deep.equal(['jim-bob'], 'method did not return superset name for substring case insenstively');
+		});
+
+		it('returns false if a name is not found', () => {
+			var list = [
+				{ "name": "jim-bob" },
+				{ "name": "bob" }
+			]
+
+			var result = smashconnector.findSubstringNames("pat", list);
+			chai.expect(result).to.deep.equal([], 'method did not return empty list for no substrings found');
+		});
+
+		it('is robust to null input', () => {
+			var result = smashconnector.findSubstringNames("jim", null);
+			chai.expect(result).to.deep.equal([], 'method did not return empty set for null list input');
+
+			var list = [
+				{ "name": "jim-bob" },
+				{ "name": "bob" }
+			]
+			var result = smashconnector.findSubstringNames(null, list);
+			chai.expect(result).to.deep.equal([], 'method did not return empty set for null name input');
+		});
+	});
+
+	describe('similar name finder', () => {
+		it('returns similar name if one is found', () => {
+			var list = [
+				{ "name": "jim" },
+				{ "name": "bob" }
+			]
+
+			var result = smashconnector.findSimilarNames("jom", list);
+			chai.expect(result).to.deep.equal(['jim'], 'method did not return similar name even though one was provided');
+		});
+
+		it('returns multiple similar names if multiple are found', () => {
+			var list = [
+				{ "name": "tany" },
+				{ "name": "tiny" },
+				{ "name": "jimmy" }
+			]
+
+			var result = smashconnector.findSimilarNames("tony", list);
+			chai.expect(result).to.deep.equal(['tany', 'tiny'], 'method did not return multiple similar names even though they were provided');
+		});
+
+		it('is not case sensitive', () => {
+			var list = [
+				{ "name": "jom" },
+				{ "name": "bob" }
+			]
+
+			var result = smashconnector.findSimilarNames("JIM", list);
+			chai.expect(result).to.deep.equal(['jom'], 'method did not return similar name even though one was provided IN ALL CAPS');
+		});
+
+		it('returns false if a name is not found', () => {
+			var list = [
+				{ "name": "jim-bob" },
+				{ "name": "bob" }
+			]
+
+			var result = smashconnector.findSimilarNames("dilbert", list);
+			chai.expect(result).to.deep.equal([], 'method did not return empty list for no similar names provided');
+		});
+
+		it('is robust to null input', () => {
+			var result = smashconnector.findSimilarNames("jim", null);
+			chai.expect(result).to.deep.equal([], 'method did not return empty set for null list input');
+
+			var list = [
+				{ "name": "jim-bob" },
+				{ "name": "bob" }
+			]
+			var result = smashconnector.findSimilarNames(null, list);
+			chai.expect(result).to.deep.equal([], 'method did not return empty set for null name input');
+		});
+	});
+
+	describe('name validation and suggestion', () => {
+		it('returns no match when given null input', () => {
+			var nullResult = {
+				"match": false,
+				"suggestions": []
+			};
+
+			var result = smashconnector.match("jim", null);
+			chai.expect(result).to.deep.equal(nullResult, 'method did not return false match for null list input');
+
+			var list = [
+				{ "name": "jim-bob" },
+				{ "name": "bob" }
+			]
+			var result = smashconnector.match(null, list);
+			chai.expect(result).to.deep.equal(nullResult, 'method did not return false match for null name input');
+		})
+	});
+});
+
+describe('data formatting', () => {
+	describe('participant formatting', () => {
+		it('formats participant data correctly', () => {
+			var expectedParticipantData = {
+				"name": "drew",
+				"character": "fox",
+				"starting_stocks": 4,
+				"remaining_stocks": 4,
+				"win": true
+			};
+			var generatedParticipantData = smashconnector.formatParticipantData("drew", "fox", 4, 4, true);
+
+			chai.expect(generatedParticipantData).to.deep.equal(expectedParticipantData, 'stage data was not formatted as expected');
+		});
+
+		it('returns an error given an empty input', () => {
+			var generatedParticipantData = smashconnector.formatParticipantData(null, "fox", 4, 4, true);
+			chai.expect(generatedParticipantData).to.be.an('error', 'did not return an error given a null input');
+
+			var generatedParticipantData = smashconnector.formatParticipantData("", "fox", 4, 4, true);
+			chai.expect(generatedParticipantData).to.be.an('error', 'did not return an error given an empty string as input');
+		});
+	});
+
+	describe('stage formatting', () => {
+		it('formats stage data correctly', () => {
+			var stage = "final_destination";
+			var chooser = "random";
+
+			var generatedStageData = smashconnector.formatStageData(stage, chooser);
+			var expectedStageData = {
+				"name": stage,
+				"chooser": chooser
+			}
+
+			chai.expect(generatedStageData).to.deep.equal(expectedStageData, 'stage data was not formatted as expected');
+		});
+
+		it('returns an error given an empty input', () => {
+			var generatedStageData = smashconnector.formatStageData(null, "random");
+			chai.expect(generatedStageData).to.be.an('error', 'did not return an error given a null input');
+
+			var generatedStageData = smashconnector.formatStageData("", "random");
+			chai.expect(generatedStageData).to.be.an('error', 'did not return an error given an empty string as input');
+		});
+	});
+
+	describe('game formatting', () => {
+		it('formats game data correctly', () => {
+			//pulled these out into helper functions since they're a little big and boilerplate
+			var generatedGameData = generateGameData();
+			var expectedGameData = getGameExpectedData();
+			chai.expect(generatedGameData).to.deep.equal(expectedGameData, 'game data was not formatted as expected');
+		});
+
+		it('returns an error given an empty input', () => {
+			var tmp = generateGameData();
+			var generatedGameData = smashconnector.formatGameData([tmp.participants[0], tmp.participants[1]], tmp.stage, null);
+			chai.expect(generatedGameData).to.be.an('error', 'did not return an error given a null input');
+
+			var generatedGameData = smashconnector.formatGameData([tmp.participants[0], tmp.participants[1]], tmp.stage, "");
+			chai.expect(generatedGameData).to.be.an('error', 'did not return an error given an empty string as input');
+		});
+
+		it('returns an error given only one participant', () => {
+			var tmp = generateGameData();
+			var generatedGameData = smashconnector.formatGameData(tmp.participants[0], tmp.stage, tmp.gameType);
+			chai.expect(generatedGameData).to.be.an('error', 'did not return an error given only a single participant');
+		});
+	});
+});
+
+function generateGameData() {
+	var participant1 = {
+		"name": "drew",
+		"character": "fox",
+		"starting_stocks": 4,
+		"remaining_stocks": 1,
+		"win": true
+	}
+	var participant2 = {
+		"name": "jeff",
+		"character": "marth",
+		"starting_stocks": 4,
+		"remaining_stocks": 0,
+		"win": false	
+	}
+	var stage = {
+		"name": "dreamland",
+		"chooser": "random"
+	}
+	var game = smashconnector.formatGameData([participant1, participant2], stage, "singles");
+
+	return game;
+}
+
+function getGameExpectedData() {
+	return {
+		"participants": [
+			{
+				"name": "drew",
+				"character": "fox",
+				"starting_stocks": 4,
+				"remaining_stocks": 1,
+				"win": true
+			},
+			{
+				"name": "jeff",
+				"character": "marth",
+				"starting_stocks": 4,
+				"remaining_stocks": 0,
+				"win": false					
+			}
+		],
+		"stage": {
+			"name": "dreamland",
+			"chooser": "random"
+		},
+		"gameType": "singles"
+	}
+}
